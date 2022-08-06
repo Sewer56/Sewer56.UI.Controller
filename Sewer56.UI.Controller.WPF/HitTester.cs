c6f1c8a2cfacc2812757e638281ff55d9608885b
Sewer 56 <seweratwork@gmail.com>
2022-08-06 23:09:27 +0100

    Improved: WPF Will no Longer make non-clickable buttons selectable.

---
+++ Sewer56.UI.Controller.WPF/HitTester.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Sewer56.UI.Controller.WPF;
+
+/// <summary>
+/// Utility for hit testing objects to determine if they are clickable.
+/// </summary>
+internal static class HitTester
+{
+    /// <summary>
+    /// Checks if a given element of Type T is at least partially clickable.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="container">The container to check if the control is hittable within.</param>
+    /// <param name="element">The element to check if it can be hit.</param>
+    /// <returns>True if the element can be clicked with the mouse.</returns>
+    internal static bool IsElementClickable(UIElement container, UIElement element)
+    {
+        var absolutePos = GetRelativePlacement((FrameworkElement)container, (FrameworkElement)element);
+        return IsPointClickable(container, element, new Point(absolutePos.TopLeft.X + 1, absolutePos.TopLeft.Y + 1)) || // Top left
+               IsPointClickable(container, element, new Point(absolutePos.BottomLeft.X + 1, absolutePos.BottomLeft.Y - 1)) || // Bottom left
+               IsPointClickable(container, element, new Point(absolutePos.TopRight.X - 1, absolutePos.TopRight.Y + 1)) || // Top right
+               IsPointClickable(container, element, new Point(absolutePos.BottomRight.X - 1, absolutePos.BottomRight.Y - 1)) || // Bottom right
+               IsPointClickable(container, element, new Point(absolutePos.TopLeft.X + absolutePos.Width / 2, absolutePos.TopLeft.Y + absolutePos.Height / 2)); // Middle
+    }
+
+    /// <summary>
+    /// Determines if a given point can be clicked by the mouse.
+    /// </summary>
+    /// <param name="container">The container relative to which the position of the element should be determined..</param>
+    /// <param name="element">The element whose position to get.</param>
+    /// <param name="position">The position to test in screen coordinates.</param>
+    /// <returns>Whether clicking at given point will hit the item.</returns>
+    private static bool IsPointClickable(UIElement container, UIElement element, Point position)
+    {
+        var hitTestResult = HitTest(position, container, element.GetType());
+        if (hitTestResult != null)
+            return WpfUtilities.IsElementChild(element, hitTestResult);
+
+        return false;
+    }
+
+    /// <summary>
+    /// Performs a hit test to determine the element hit if clicking at a position.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="position">The position to check.</param>
+    /// <param name="container">The container to check.</param>
+    /// <param name="desiredType">Type of item to hit test for.</param>
+    /// <returns>The object hit.</returns>
+    private static DependencyObject? HitTest(Point position, UIElement container, Type desiredType)
+    {
+        DependencyObject? result = null;
+        HitTestResultBehavior ResultCallback(HitTestResult hitTestResult)
+        {
+            bool isDesiredType = hitTestResult.VisualHit.GetType() == desiredType;
+            if (!isDesiredType && WpfUtilities.FindParent(hitTestResult.VisualHit, desiredType) == null)
+                return HitTestResultBehavior.Continue;
+
+            result = hitTestResult.VisualHit;
+            return HitTestResultBehavior.Stop;
+        }
+
+        var hitTestResult = container.InputHitTest(position);
+        if (hitTestResult is DependencyObject obj && (hitTestResult.GetType() == desiredType || WpfUtilities.FindParent(obj, desiredType) != null))
+            return obj;
+
+        return null;
+    }
+
+    /// <summary>
+    /// Retrieves the placement of the element relative to the container.
+    /// </summary>
+    /// <param name="container">The container relative to which the position of the element should be determined..</param>
+    /// <param name="element">The element whose position to get.</param>
+    private static Rect GetRelativePlacement(FrameworkElement container, FrameworkElement element)
+    {
+        var elementPos   = element.PointToScreen(new Point(0, 0));
+        var containerPos = container.PointToScreen(new Point(0, 0));
+        var relativePos  = new Point(elementPos.X - containerPos.X, elementPos.Y - containerPos.Y);
+        return new Rect(relativePos.X, relativePos.Y, element.ActualWidth, element.ActualHeight);
+    }
+}
--- Sewer56.UI.Controller.WPF/WpfUtilities.cs
+++ Sewer56.UI.Controller.WPF/WpfUtilities.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Windows.Media;
 using System.Windows;
 using System.Windows.Input;
+using System.Collections.Generic;
 
 namespace Sewer56.UI.Controller.WPF;
 
@@ -32,19 +33,29 @@ public static class WpfUtilities
     /// <param name="item">Current item in the recursion hierarchy.</param>
     /// <param name="exclude">Item to exclude (if present).</param>
     /// <param name="accumulator">The list that will receive all of the child UI items.</param>
-    public static void FindSelectableChildren(DependencyObject item, UIElement exclude, ref SpanList<UIElement> accumulator)
+    public static void FindSelectableChildren(DependencyObject item, UIElement exclude, ref SpanList<UIElement> accumulator) => FindSelectableChildrenEx(item, item, exclude, ref accumulator);
+
+    /// <summary>
+    /// Finds all children of the current element using recursion.
+    /// </summary>
+    /// <param name="parent">The parent/root from wihch we are searching from.</param>
+    /// <param name="item">Current item in the recursion hierarchy.</param>
+    /// <param name="exclude">Item to exclude (if present).</param>
+    /// <param name="accumulator">The list that will receive all of the child UI items.</param>
+    public static void FindSelectableChildrenEx(DependencyObject parent, DependencyObject item, UIElement exclude, ref SpanList<UIElement> accumulator)
     {
         var children = VisualTreeHelper.GetChildrenCount(item);
         for (int x = 0; x < children; x++)
         {
             var child = VisualTreeHelper.GetChild(item, x);
-            if (child == null) 
+            if (child == null!)
                 continue;
 
-            if (child is UIElement { Focusable: true, IsVisible: true, IsEnabled: true } uiElement && uiElement != exclude)
+            if (child is UIElement { Focusable: true, IsVisible: true, IsEnabled: true } uiElement && uiElement != exclude 
+                && HitTester.IsElementClickable((UIElement)parent, uiElement))
                 accumulator.Add(uiElement);
 
-            FindSelectableChildren(child, exclude, ref accumulator);
+            FindSelectableChildrenEx(parent, child, exclude, ref accumulator);
         }
     }
 
@@ -64,6 +75,29 @@ public static class WpfUtilities
     }
 
     /// <summary>
+    /// Tries to find a parent with a given type in the visual tree hierarchy.
+    /// </summary>
+    /// <param name="item">Current item in the visual tree hierarchy.</param>
+    /// <param name="type">The type of parent to find.</param>
+    public static object? FindParent(DependencyObject item, Type type)
+    {
+        try
+        {
+            var parent = VisualTreeHelper.GetParent(item);
+            if (parent == null)
+                return default;
+
+            return parent.GetType() == type ? parent
+                                            : FindParent(parent, type);
+        }
+        catch (Exception)
+        {
+            // FlowDocument, etc.
+            return default;
+        }
+    }
+
+    /// <summary>
     /// Tries to get the currently focused element and its corresponding window.
     /// </summary>
     /// <param name="window">The currently selected window.</param>
@@ -79,6 +113,46 @@ public static class WpfUtilities
 
         window = Window.GetWindow(focused);
         return window != null;
+    }
+
+    /// <summary>
+    /// Determines if an item is a child of a given parent.
+    /// </summary>
+    /// <param name="child">The child to check if within parent.</param>
+    /// <param name="parent">The parent element.</param>
+    public static bool IsElementChild(DependencyObject parent, DependencyObject child)
+    {
+        if (child.GetHashCode() == parent.GetHashCode())
+            return true;
+
+        foreach (DependencyObject parentChild in EnumerateChildren<DependencyObject>(parent))
+        {
+            if (parentChild.GetHashCode() == child.GetHashCode())
+                return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Enumerates list of all the children of the given object.
+    /// </summary>
+    /// <typeparam name="T">The type of child to return.</typeparam>
+    /// <param name="parent">The parent to check children of.</param>
+    public static IEnumerable<T> EnumerateChildren<T>(DependencyObject? parent) where T : DependencyObject
+    {
+        if (parent == null)
+            yield break;
+
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+        {
+            DependencyObject? child = VisualTreeHelper.GetChild(parent, i);
+            if (child != null! && child is T)
+                yield return (T)child;
+
+            foreach (T childOfChild in EnumerateChildren<T>(child))
+                yield return childOfChild;
+        }
     }
 }
 
